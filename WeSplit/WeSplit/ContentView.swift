a6454368217a1435405be488fed916958e6a2134
Alexey <alex0klain@gmail.com>
2020-01-15 12:53:59 +0300

    Add main UI and Logic of the App

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,50 +9,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = 0
-//    @State private var tapCount = 0
-//    @State private var name = ""
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
     
     var body: some View {
-        
-//        NavigationView {
-//            Form {
-//                Section {
-//                    Text("Hello, World!")
-//                    Text("Hello, World!")
-//                    Text("Hello, World!")
-//                }
-//                Section {
-//                    Text("Hello, World!")
-//                    Text("Hello, World!")
-//                    Text("Hello, World!")
-//                }
-//            }.navigationBarTitle("SwiftUI")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
                 
-//        Button("Tap count: \(tapCount)") {
-//            self.tapCount += 1
-//        }
-        
-//        Form {
-//            TextField("Enter your name", text: $name)
-//            Text("Your name is \(name)")
-//        }
-            
-//        }
-//        Form {
-//            ForEach(0 ..< 100) {
-//                Text("Row \($0)")
-//            }
-//        }
-        
-        VStack {
-            Picker("Select your student", selection: $selectedStudent) {
-                ForEach(0 ..< students.count) {
-                    Text(self.students[$0])
+                Section(header: Text("How much tip do you want to leave?"), content: {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                })
+                
+                Section {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
                 }
             }
-            Text("You chose: Student # \(students[selectedStudent])")
+            .navigationBarTitle("WeSplit")
         }
     }
 }
