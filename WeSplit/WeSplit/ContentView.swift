e2011a3d22c02ad531e675cba99d54278683c1af
Alexey <alex0klain@gmail.com>
2020-01-16 12:59:17 +0300

    Add header to the third section, add another section showing the total amount for the check, change the 'Number of people' picker to be a text field

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -10,18 +10,23 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = ""
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = ""
     @State private var tipPercentage = 2
     let tipPercentages = [10, 15, 20, 25, 0]
     
-    var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
+    var totalAmount: Double {
         let tipSelection = Double(tipPercentages[tipPercentage])
         let orderAmount = Double(checkAmount) ?? 0
         
         let tipValue = orderAmount / 100 * tipSelection
         let grandTotal = orderAmount + tipValue
-        let amountPerPerson = grandTotal / peopleCount
+        
+        return grandTotal
+    }
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople) ?? 1
+        let amountPerPerson = totalAmount / peopleCount
         
         return amountPerPerson
     }
@@ -33,11 +38,13 @@ struct ContentView: View {
                     TextField("Amount", text: $checkAmount)
                         .keyboardType(.decimalPad)
                     
-                    Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2 ..< 100) {
-                            Text("\($0) people")
-                        }
-                    }
+//                    Picker("Number of people", selection: $numberOfPeople) {
+//                        ForEach(2 ..< 100) {
+//                            Text("\($0) people")
+//                        }
+//                    }
+                    TextField("Number of people", text: $numberOfPeople)
+                        .keyboardType(.numberPad)
                 }
                 
                 Section(header: Text("How much tip do you want to leave?"), content: {
@@ -48,9 +55,13 @@ struct ContentView: View {
                     }.pickerStyle(SegmentedPickerStyle())
                 })
                 
-                Section {
+                Section(header: Text("Total amount"), content: {
+                    Text("$\(totalAmount, specifier: "%.2f")")
+                })
+                
+                Section(header: Text("Amount per person"), content: {
                     Text("$\(totalPerPerson, specifier: "%.2f")")
-                }
+                })
             }
             .navigationBarTitle("WeSplit")
         }
